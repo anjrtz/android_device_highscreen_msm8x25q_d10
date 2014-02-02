9c46be6415d7592e51d9f03ecaf7264a2893ceb2
Yaroslav Sidlovsky <zawertun@gmail.com>
2014-02-02 13:56:54 +0400

    Work in progress

--- cm.mk
+++ cm.mk
@@ -1,6 +1,10 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Release name
 PRODUCT_RELEASE_NAME := msm8x25q_d10
 
@@ -14,5 +18,5 @@ $(call inherit-product, device/highscreen/msm8x25q_d10/device_msm8x25q_d10.mk)
 PRODUCT_DEVICE := msm8x25q_d10
 PRODUCT_NAME := cm_msm8x25q_d10
 PRODUCT_BRAND := highscreen
-PRODUCT_MODEL := msm8x25q_d10
-PRODUCT_MANUFACTURER := highscreen
+PRODUCT_MODEL := Boost II
+PRODUCT_MANUFACTURER := Highscreen
--- device_msm8x25q_d10.mk
+++ device_msm8x25q_d10.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
-$(call inherit-product-if-exists, vendor/highscreen/msm8x25q_d10/msm8x25q_d10-vendor.mk)
+$(call inherit-product-if-exists, vendor/highscreen/msm8x25q_d10/BoardConfigVendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/highscreen/msm8x25q_d10/overlay
 
@@ -17,8 +17,13 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, build/target/product/full_base.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_msm8x25q_d10
 PRODUCT_DEVICE := msm8x25q_d10
+
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_LOCALES := en_US ru_RU
